142e4e6cbc109bb37c705daa856b6462689b3ef8
あで <adelaide@fiberplane.com>
2024-02-29 00:38:32 +0100

    Release 0.1.0

--- main/index.js
+++ main/index.js
@@ -10,14 +10,20 @@ var __webpack_exports__ = {};
 const external_fs_namespaceObject = __WEBPACK_EXTERNAL_createRequire(import.meta.url)("fs");
 ;// CONCATENATED MODULE: external "os"
 const external_os_namespaceObject = __WEBPACK_EXTERNAL_createRequire(import.meta.url)("os");
-;// CONCATENATED MODULE: ./src/const.ts
+;// CONCATENATED MODULE: external "perf_hooks"
+const external_perf_hooks_namespaceObject = __WEBPACK_EXTERNAL_createRequire(import.meta.url)("perf_hooks");
+;// CONCATENATED MODULE: ./src/metrics/const.ts
 const TMP_VAR_NAME = "AUTOMETRICS_START";
+const TMP_VAR_GITHUB_NAME = `STATE_${TMP_VAR_NAME}`;
+const HISTOGRAM_NAME = "workflow.jobs.duration";
+const COUNTER_NAME = "workflow.jobs.count";
 
 ;// CONCATENATED MODULE: ./src/main.ts
 
 
 
-external_fs_namespaceObject.appendFileSync(process.env.GITHUB_STATE, `${TMP_VAR_NAME}=${new Date().valueOf()}${external_os_namespaceObject.EOL}`, {
+
+(0,external_fs_namespaceObject.appendFileSync)(process.env.GITHUB_STATE, `${TMP_VAR_NAME}=${external_perf_hooks_namespaceObject.performance.now()}${external_os_namespaceObject.EOL}`, {
     encoding: "utf8",
 });
 
